64179da218765d116d00dec085fd4f7ddc474048
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-11-07 15:49:59 +0100

    Initial Commit

---
+++ TestTaskSfUI/TestTaskSfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskSfUIApp.swift
+//  TestTaskSfUI
+//
+//  Created by user246073 on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskSfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
